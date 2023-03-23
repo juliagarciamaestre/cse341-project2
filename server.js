f5274f47daff5c3131b41833758c2b78f1b3fb25
Julia García <juliagarciamaestre@gmail.com>
2023-03-23 17:05:15 -0300

    Lesson 7 Error handler in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
+const createError = require('http-errors');
 
 
 const app = express();
@@ -15,6 +16,23 @@ app.use((req, res, next) =>{
 
 app.use('/', require('./routes'));
 
+//404 handler and pass to error handler
+app.use((req, res, next) => {
+    next(createError(404, "Not found"));
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 5000);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    });
+});
+
+//Mongo connection
 mongodb.initDb((error, mongodb) =>{
     if (error){
         console.log(error);
